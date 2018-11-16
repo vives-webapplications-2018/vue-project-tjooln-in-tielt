808b5c42c2e3d17bb473180da7e563bb15262fe6
Arthur Verstraete <verstraetearthur@gmail.com>
2018-11-16 12:01:30 +0100

    basic mqtt

---
+++ config.js
@@ -0,0 +1,6 @@
+websocketserver = 'broker.mqttdashboard.com';
+websocketport = 8000;
+// websocketserver = 'm23.cloudmqtt.com';
+// websocketport = 11239;
+// websocketport = 11239;
+// websocketport = 11239;
