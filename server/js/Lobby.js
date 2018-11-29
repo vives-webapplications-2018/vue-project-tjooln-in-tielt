e7d06ba0e37d01b535e4986dfc8f150383cc618f
Robin Spruytte <robin.spruytte646@gmail.com>
2018-11-30 00:54:26 +0100

    added addPlayer function to Lobby.js

--- server/js/Lobby.js
+++ server/js/Lobby.js
@@ -5,6 +5,15 @@ function Lobby(lobbyName, maxPlayers){      //expects string:lobbyName, int:maxP
     this.name = lobbyName;
     this.playerList = [];
     this.maxPlayers = maxPlayers;
+
+    this.addPlayer = function(playerName) {     //expects string:playerName, adds to the playerList if not full, returns true if player was able to join else false
+        if (self.isFull()){
+            self.playerList.push(playerName);
+            return true;
+        } else {
+            return false;
+        }
+    };
 }
 
 module.exports = Lobby;
