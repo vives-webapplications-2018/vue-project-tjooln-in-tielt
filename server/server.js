07cb6dda1be37f5ddff0c241baf1382db95dac5c
Robin Spruytte <robin.spruytte646@gmail.com>
2018-11-30 00:45:48 +0100

    replaced test routes with /getimage route on server.js

--- server/server.js
+++ server/server.js
@@ -9,12 +9,13 @@ const db = require('./js/db.js');
 const path = require('path');
 
 const app = express();
-const mqtt = new Mqtt('mqtt://broker.mqttdashboard.com','scribble/lobby', messageHandler);
 
 //responsive requires (server to client communication)
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var expressPublicIp = require('express-public-ip');  //needed to make server public
+var LobbyList = new LobbyList('lobby', 100);
+var messageController = new MessageController('mqtt://broker.mqttdashboard.com', 'scribble/lobby', "!", db, LobbyList);
 
 app.enable('trust proxy');
  
@@ -26,19 +27,15 @@ var bodyParser = require('body-parser');
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+setupDb();
 
 //server connection related
 
 app.listen(3000,"0.0.0.0");                         //start listening on port 3000
 
-app.get('/', function (req, res) {
-    testDbFunctions(req.ip);
-    res.send("Hello there your precense was noted " + req.ip);
-});
-
-app.get('/returnImage', function (req, res) {
-    testDbFunctions(req.ip);
-    res.send("Hello there your precense was noted " + req.ip);
+//example request: http://localhost:3000/getImage?fileName=auto.jpg
+app.get('/getimage', function (req, res) {
+    res.sendFile(__dirname + "/public/images/" + req.query.fileName);
 });
 
 
