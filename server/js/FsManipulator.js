765558f9a6fe4b4382e015cebb4317ced6802913
Robin Spruytte <robin.spruytte646@gmail.com>
2018-11-30 01:09:43 +0100

    removed duplicate FsManipulator

--- server/js/FsManipulator.js
+++
@@ -1,9 +0,0 @@
-
-const fs = require('fs');                                    // require file system
-
-exports.getFilesInFolder = function(path, callback) {        //expects a path to the folder and a callback
-    fs.readdir(path, function (err, items) {
-        if (err) return callback(err);
-        callback(null, items);                              //returns an array of the found files to the callback
-    });
-};
