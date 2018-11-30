ccf98fb761b0cbd65607900b8b632dcabcc2ea84
Robin Spruytte <robin.spruytte646@gmail.com>
2018-11-30 01:05:09 +0100

    added parseMessage function to MessageController.js

--- server/js/MessageController.js
+++ server/js/MessageController.js
@@ -14,6 +14,19 @@ function MessageController(brokerURL, initialTopic, commandPrefix, db, playerLis
         console.log('i received a message on ' + topic + ' with content ' + message);
         self.parseMessage(topic, message);
     });
+
+    this.parseMessage = function(topic, message) {                      //expects string:topic and string:message, searches for the right command to execute
+        var splitInformation = message.split(" ");
+        
+        if (splitInformation.length >= 2) {                             //if message has at least 2 words (command and prefix)
+            if (splitInformation[0] == commandPrefix){                  //if first word is prefix
+                splitInformation.shift();                               //remove prefix
+                if ( self.commandList.includes(splitInformation[0]) ){  //if command exists
+                    commands[splitInformation[0]](splitInformation, topic, mqtt, self.db, self.playerList); //call command and give argument list, topic and the messageController
+                }
+            }
+        }
+    };
 }
 
 module.exports = MessageController;
