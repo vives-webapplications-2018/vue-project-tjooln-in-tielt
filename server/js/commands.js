deab17db7001f1d950584aea08a6ddce3f13828c
Robin Spruytte <robin.spruytte646@gmail.com>
2018-11-30 01:11:06 +0100

    added addPlayer function to commands.js

--- server/js/commands.js
+++ server/js/commands.js
@@ -1,1 +1,20 @@
 //LISTS ALL AVAILABLE SERVER COMMANDS
+
+
+//sampleCommand: ! addPlayer playerName lobby
+exports.addPlayer = (arguments, topic, mqtt, db, playerList) => {   //expects string:playerName and string:parentTopic, server creates this topic and player sends message to player to subscribe on sucsess, server adds player to list
+    let playerName = arguments[1];
+    let lobby = arguments[2];
+    let newTopic = "scribble/" + lobby + "/" + playerName;
+
+    if (playerList.getLobbyList().includes(lobby)){
+        if ( playerList.lobbyList[lobby].addPlayer(playerName) ){
+            mqtt.subscribeTopic(newTopic);
+            mqtt.send(topic, "subscribe " + playerName + " " + newTopic);
+        } else {
+            mqtt.send(topic, "error " + lobby + "_full");
+        }
+    } else {
+        mqtt.send(topic, "error unknown_lobby_" + lobby);
+    }
+};
