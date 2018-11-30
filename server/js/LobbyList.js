8a961d073df047b1018d40f1a8fe0d572596d549
Robin Spruytte <robin.spruytte646@gmail.com>
2018-11-30 01:00:20 +0100

    added addLobby function to LobbyList.js

--- server/js/LobbyList.js
+++ server/js/LobbyList.js
@@ -6,6 +6,12 @@ function LobbyList(mainLobbyName, maxTotalPlayers){ //expects string:mainLobbyNa
     var self = this;
     this.lobbyList = [];
     this.lobbyList[mainLobbyName] = new Lobby(mainLobbyName, maxTotalPlayers); //initialises main lobby
+
+    this.addLobby = function(lobbyName, maxPlayers) { //expects string:lobbyName, int:maxPlayers adds a Lobby to lobbylist with specified variables
+        if (!self.lobbyList.includes(lobbyName)){
+            self.lobbyList[lobbyName] = new Lobby(lobbyName, maxPlayers);
+        }
+    };
 }
 
 module.exports = LobbyList;
