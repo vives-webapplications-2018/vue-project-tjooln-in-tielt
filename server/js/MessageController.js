c97f9b86a1c2776ece24bfd0128c6632d47a68e3
Robin Spruytte <robin.spruytte646@gmail.com>
2018-11-30 01:03:28 +0100

    added dependencies to MessageController.js

--- server/js/MessageController.js
+++ server/js/MessageController.js
@@ -1,5 +1,7 @@
 //HANDLES ALL MQTT TRAFFIC
 
+var Mqtt = require('../js/Mqtt.js');
+var commands =require('../js/commands.js');
 
 //exampleMessage ! addLobby lobbyName
 function MessageController(brokerURL, initialTopic, commandPrefix, db, playerList){
